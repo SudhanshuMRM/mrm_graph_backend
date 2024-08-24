68a3b305c5968d1e3076e18c6ce42ef314d78c70
SudhanshuMRM <sudhanshu@mrmprocom.com>
2024-08-24 11:33:53 +0530

    mongoDB connection is done, model is comming and dummy api is getting data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cron = require('node-cron');
 const mqttConnect = require('./utils/helper')
+const User = require('./utils/model')
 
 
 const app = express();
@@ -13,15 +14,12 @@ app.use(express.json()); // For parsing application/json
 
 //daily call the function to fetch data
 
-const dailyTask = () =>{console.log("daily task called")}
+const dailyTask = () => { console.log("daily task called") }
 
-// cron.schedule('0 0 * * *', () => {
-//     dailyTask();
-// });
+cron.schedule('0 0 * * *', () => {
+    dailyTask();
+});
 
-// cron.schedule('55 9 * * *', () => {
-//     dailyTask();
-// });
 
 
 // Routes
@@ -30,21 +28,40 @@ app.get('/api/dummy', (req, res) => {
     console.log("Dummy API Tested");
 });
 
-app.post('/api/dummy2', (req, res) => {
-    const body = req.body; // No need to await
-    console.log("Received body:", body);
- 
+app.get('/api/getDummyData', async (req, res) => {
+    try {
+        // Connect to MongoDB
+        const resp = await mongoose.connect("mongodb+srv://sudhanshu:hjPukpCKLzuSmw1Q@mrmgraphs.rnumk.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=MRMGraphs");
 
-    res.json({ message: body });
+        if (resp && resp.connection.readyState === 1) {
+            console.log("Connected to MongoDB successfully");
+            // Fetch data from the collection
+            const users = await User.find({}); // This will fetch all documents in the 'users' collection
 
+            res.json(users);
+        } else {
+            console.error("Failed to establish a connection to MongoDB");
+            res.status(500).json({ message: 'Failed to connect to MongoDB' });
+        }
+
+
+
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ message: 'Error fetching data' });
+    } finally {
+        // Close the connection after the query is complete
+        mongoose.connection.close();
+    }
 });
 
-app.post('/api/getGraphData',async(req, res) => {
+app.post('/api/getGraphData', async (req, res) => {
     const url = "http://192.168.4.1/api/v1.0/keys/attributes"
     const body = req.body; // No need to await
     // const deviceID = body.device_id;
     // console.log("Device Id:", deviceID);
-    const resp = await  mqttConnect(body.device_id,url);
+    const resp = await mqttConnect(body.device_id, url);
     res.json({ message: resp });
 });
 
@@ -81,3 +98,7 @@ app.use((err, req, res, next) => {
 });
 
 
+
+// mongoUserName - sudhanshu
+// mongoPassword-hjPukpCKLzuSmw1Q
+// mongo connection string - mongodb+srv://sudhanshu:hjPukpCKLzuSmw1Q@mrmgraphs.rnumk.mongodb.net/?retryWrites=true&w=majority&appName=MRMGraphs
